6c304b8e7cac7795bc6fede91f3fbd44874e79f7
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 16:45:02 +0530

    Add: Starting simulator and displaying head or tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,14 @@
 #!/bin/bash -x
+
+isHead=0
+isTail=1
+
+coinSimulator=$(($RANDOM%2))
+
+if [ $coinSimulator -eq $isHead ]
+then
+        echo " Head is the Winner "
+else
+        echo " Tail is the Winner "
+fi
+
