a44f31212d1fef62e5f4316037fdc22e35fc8733
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 16:59:07 +0530

    Add: Flipping coin multiple times and displaying win count

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,13 +2,23 @@
 
 isHead=0
 isTail=1
+headCounter=0
+tailCounter=0
 
-coinSimulator=$(($RANDOM%2))
+for((i=0; i<=41; i++))
+do
+        coinSimulator=$(($RANDOM%2))
 
-if [ $coinSimulator -eq $isHead ]
-then
-        echo " Head is the Winner "
-else
-        echo " Tail is the Winner "
-fi
+        if [ $coinSimulator -eq $isHead ]
+        then
+                ((headCounter++))
+                headCounter=$headCounter
+        else
+                ((tailCounter++))
+                tailCounter=$tailCounter
+        fi
+done
+
+echo " Head Won : $headCounter times "
+echo " Tail Won : $tailCounter times "
 
